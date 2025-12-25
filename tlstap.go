88da8d4a72cfc73ce368220aea5132b210b379ea
random <random@kali.hacking>
2025-12-25 01:24:52 +0100

    Cleanup main

--- tlstap.go
+++ tlstap.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"encoding/hex"
-	"log"
 	"tlstap/cli"
-	"tlstap/intercept"
 )
 
 func main() {
-	data := []byte("hello proxy")
-	info := intercept.DataFrame{
-		Size:   uint32(len(data)),
-		ConnId: 17,
-	}
-
-	s := hex.EncodeToString(info.ToBytes())
-	log.Printf("%s%s", s, hex.EncodeToString(data))
-
 	cli.StartWithCli(nil)
 }
